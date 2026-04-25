fa5d3e7d5ef0dc17780055f3c99a991efba1c12f
Genesis <kirilukvladweb10@gmail.com>
2026-04-25 12:54:37 +0300

    lab1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello World</title>
+</head>
+<body>
+
+<header>
+    <img src="photo/mospolitech.png" alt="logo" class="logo">
+    <h2>Hello, World!</h2>
+</header>
+
+<main>
+    <?php echo "Hello, World!"; ?>
+</main>
+
+<footer>
+    <p>задание для самостоятельной работы</p>
+</footer>
+
+</body>
+</html>
